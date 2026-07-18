7595f6805a6c087661a50d2703e038ba6af2946e
Bikash Timsina <2341bikash67@gmail.com>
2026-07-18 19:54:31 +0545

    About

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Welcome to Bikash Timsina's Portfolio 🚀");
+const text = [
+    "Aspiring Software Engineer",
+    "Learning C# & JavaScript",
+    "Exploring Artificial Intelligence"
+];
+
+let index = 0;
+let char = 0;
+
+function typing(){
+
+    if(char < text[index].length){
+
+        document.getElementById("typing").innerHTML += text[index].charAt(char);
+        char++;
+        setTimeout(typing,80);
+
+    }
+
+    else{
+
+        setTimeout(()=>{
+            document.getElementById("typing").innerHTML="";
+            char=0;
+            index++;
+
+            if(index >= text.length){
+                index=0;
+            }
+
+            typing();
+
+        },1000);
+
+    }
+
+}
+
+
+typing();
